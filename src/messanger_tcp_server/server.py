220f0b424bafbaa19009126c26cdda3698999691
Anbu-A <82471581+Anbu-A@users.noreply.github.com>
2021-04-26 15:13:33 +0200

    removed old code
    also added a line to remove the dict. entry from the disconnected client.

--- src/messanger_tcp_server/server.py
+++ src/messanger_tcp_server/server.py
@@ -43,25 +43,16 @@ class Server():
         self.client_names[client] = client_name
         print(client_name)
 
-        '''while True:
-            while(True):
-                client_msg = client.recv(self.buffer_size).decode()
-                print(f"User {str(client_name)}: {client_msg}")
-
-                for clients in self.connected_clients:
-                    if(clients != client):
-                        clients.send((f"[{client_name}]:{client_msg}\n").encode())               
-                break'''
-
         while(True):
             client_msg = client.recv(self.buffer_size).decode()
             # https://stackoverflow.com/questions/667640/how-to-tell-if-a-connection-is-dead-in-python
             if(len(client_msg) == 0):
+                del self.client_names[client]
                 self.connected_clients.remove(client)
                 client.close()
                 print(f"Clients left: {len(self.connected_clients)}")
                 break
-            # TO-DO: Also close connection from server side if client unexpectetlly DCs 
+
             print(f"User {str(client_name)}: {client_msg}")
             for clients in self.connected_clients:
                 if(clients != client):
